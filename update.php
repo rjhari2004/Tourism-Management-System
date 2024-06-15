bec499ee1704fffbc0240e0dd71c0db8b6e44474
rjhari <121494956+rjhari2004@users.noreply.github.com>
2024-06-15 13:50:01 +0530

    update.php

---
+++ update.php
@@ -0,0 +1,30 @@
+<?php
+	include('conn.php');
+	$id=$_GET['id'];
+	$ss=$_POST['sstation'];
+	$es=$_POST['estation'];
+	$tn=$_POST['tn'];
+	$jd=$_POST['jd'];
+	$firstname=$_POST['firstname'];
+	$lastname=$_POST['lastname'];
+    $age=$_POST['age'];
+    $gender=$_POST['gender'];
+    $coach=$_POST['coach'];
+    $seat=$_POST['seat'];
+	$f=fopen("Ticket.txt","a+");
+	fwrite($f,"The above person's ticket has been edited Below is new details \n");
+	fwrite($f,"Train Ticket\n");
+			fwrite($f,"Starting Station :".$ss."\n");
+			fwrite($f,"End Station :".$es."\n");
+			fwrite($f,"Train name :".$tn."\n");
+			fwrite($f,"Journey date :".$jd."\n");
+		fwrite($f,"First Name :".$firstname."\n");
+		fwrite($f,"Last Name :".$lastname."\n");
+		fwrite($f,"Age:".$age."\n");
+		fwrite($f,"Gender:".$gender."\n");
+		fwrite($f,"Coach:".$coach."\n");
+		fwrite($f,"Seat :".$seat."\n");
+		fclose($f);
+	mysqli_query($conn,"update `user` set firstname='$firstname', lastname='$lastname',age='$age',gender='$gender',coach='$coach',seat='$seat' where userid='$id'");
+	header('location:ticket.php');
+?>
