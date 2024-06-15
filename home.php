703c581312887e9d45b7b8bf6cb16d41674943c2
rjhari <121494956+rjhari2004@users.noreply.github.com>
2024-06-15 13:47:34 +0530

    home.php

---
+++ home.php
@@ -0,0 +1,74 @@
+<html>
+    <style>
+        p{
+			border-style:solid;
+			border-width:thick;
+			border-color:blue;
+			color:white;
+			font-size:20px;
+			background-color:#00B2C2;
+		}
+        header{
+			background-color:blue;
+			color:white;
+		}
+		ul{
+			list-style-type:none;
+			margin:0;
+			padding:0;
+			overflow:hidden;
+		}
+		
+		nav ul li{
+			dispaly: inline-block;
+			line-height: 80px;
+			margin: 0 5px;
+		}
+		li{
+			float:left;}
+		a#k:link, a#k:visited{
+			display:block;
+			font-weight:bold;
+			color:white;
+			background-color:blue;
+			width:150px;text-align:center;padding:4px;text-transform:uppercase;text-decoration:none;}
+		a#k:hover,a#k:active{
+			background-color:#7a991a;}
+        </style>
+        <header>
+	<nav>
+	<ul>
+		<li><a style="width:100px;"class="font-effect-shadow" id="k" href="home.php">HOME</a></li>
+		<li><a class="font-effect-shadow" style="width:100px;" id="k" href="maps.php">EXPLORE</a></li>
+		<li><a class="font-effect-shadow" style="width:180px;" id="k" href="ticket.php">TRAIN BOOKING</a></li>
+		<li><a class="font-effect-shadow" style="width:180px;" id="k" href="roomexp.php">EXPLORE ROOMS</a></li>
+		<li><a class="font-effect-shadow" style="width:180px;" id="k" href="bookroom.php">BOOK ROOMS</a></li>
+		</ul></nav>
+	</header>
+<body bgcolor="#ADD8E6">
+<?php
+    echo "<h1 align='center'>"."Tourism Management System"."</h1>";?>
+    <center><img border="20" src="https://tse3.mm.bing.net/th?id=OIP.YpH01oPogddYCEJOb9cgQQAAAA&pid=ImgDet&rs=1"><p>
+The country’s people & culture, its tourism defines<br>
+If no tourism, country socially declines<br>
+The country is well known if  its tourism shines<br>
+The increase in tourism are development signs<br>
+Tourist spots are country’s headlines<br>
+But first read the guidelines<br>
+You will discover variety of designs<br>
+You will love to travel<br>
+If you read my lines.<br>
+
+
+Tourism makes tradition & culture exchange<br>
+Without tourism country looks strange<br>
+Tourism tells the mode of change<br>
+Just you have to go, out of your range<br>
+See, how the beauty is arrange<br>
+You will find,<br>
+                      The peace on mountain range
+Tourism is the country’s advantage.<br>
+    This project done by R J HARI 2022BCS0125 is an attempt to help tourists chose their dream destinations and book proper tickets for rooms and travel</p>
+</center>
+</body>
+</html>
